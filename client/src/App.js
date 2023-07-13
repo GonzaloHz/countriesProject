d0e1d44a0c59e55a80e1c9416c5aeed62d802803
Gonzalo Hernandez <gonzalo.hernandez1@hotmail.com>
2023-07-13 00:58:01 -0300

    created addActivity fix designs in home and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
 import CardDetail from './Components/CardDetail/CardDetail';
+import AddActivity from "./Components/AddActivity/AddActivity";
 
 function App() {
   return(
@@ -10,6 +11,7 @@ function App() {
     <Navbar />
     <Routes>
       <Route path='/home/:id' element={<CardDetail />}/>
+      <Route path='/add-activity' element={<AddActivity />} />
       <Route path='/home' element={<Home />} />
     </Routes>
   </div>
